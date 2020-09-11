fcf2b89762e14f17bd6df7edf5610b3b299667fd
Milan Petrovic <milan@gdragon.info>
2020-09-11 23:27:00 +0200

    More basic files.

---
+++ js/debugpress.js
@@ -0,0 +1,11 @@
+/*jslint regexp: true, nomen: true, undef: true, sloppy: true, eqeq: true, vars: true, white: true, plusplus: true, maxerr: 50, indent: 4 */
+/*global debugpress_data*/
+
+;(function($, window, document, undefined) {
+	window.wp = window.wp || {};
+	window.wp.dev4press = window.wp.dev4press || {};
+
+	window.wp.dev4press.debugpress = {
+
+	};
+})(jQuery, window, document);
